6a504c0bfe932c0921f8a950af0805cf9ac84300
micheal peter <olaonipekunpeter50@yahoo.com>
2025-12-17 13:34:23 +0100

    styled login page

--- src/component/login.jsx
+++ src/component/login.jsx
@@ -1,21 +1,48 @@
+import { FingerprintPattern } from "lucide-react";
+
 export default function LoginForm({ toggleForm }) {
   return (
-    <div className="container-form">
-      <h1>Login</h1>
+    <div className="p-5">
+      <div className=" size-12 flex justify-center items-center rounded-2xl mt-10 mb-5 p-1 overflow-hidden mx-auto  bg-blue-500">
+        <FingerprintPattern  className="size-[90%] text-white " />
+        
+      </div>
+      <h1 className="text-center">Login</h1>
       <form>
         <div className="input-field">
-          <label htmlFor="username">Username</label>
-          <input type="text" id="username" required />
+          <label htmlFor="username" className="text-zinc-500 block mb-2">
+            Username
+          </label>
+          <input
+            type="text"
+            id="username"
+            className="border border-zinc-400 w-full h-10 rounded outline-0 px-2 mb-3"
+            required
+          />
         </div>
         <div className="input-field">
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" required />
+          <label htmlFor="password" className="text-zinc-500 block mb-2">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            className="border border-zinc-400 w-full h-10 rounded outline-0 px-2 mb-3"
+            required
+          />
         </div>
-        <button type="submit" className="btn-submit">
-          Sign In
+
+        <button
+          type="submit"
+          className="bg-blue-500 text-white w-full h-10 rounded mt-4 mb-3.5"
+        >
+          Sign Up
         </button>
       </form>
-      <p>Dont have an account? <button onClick={toggleForm}>Sign up</button></p>
+
+      <p className="text-center text-sm font-light">
+        Dont have an account? <button onClick={toggleForm}>Sign up</button>
+      </p>
     </div>
   );
 }
