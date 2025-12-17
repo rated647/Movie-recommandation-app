2ac9a92a93d276a2b1b4566fc2ff4b3a3f2b6f0e
micheal peter <olaonipekunpeter50@yahoo.com>
2025-12-17 13:12:02 +0100

    styled signup page

--- src/component/signup.jsx
+++ src/component/signup.jsx
@@ -1,25 +1,55 @@
+import { UserPen } from "lucide-react";
+
 export default function SignUp({ toggleForm }) {
   return (
-    <div className="container-form">
-      <h1 className="bg-blue-500 text-2xl ">Sign Up</h1>
-      <form>
+    <div className="p-5">
+      <div className=" size-12 flex justify-center items-center rounded-2xl mt-10 mb-5 p-1 overflow-hidden mx-auto  bg-blue-500">
+        <UserPen className="size-[90%] text-white " />
+      </div>
+      <h1 className=" text-2xl text-center  mb-10 ">Sign Up</h1>
+      <form className="">
         <div className="input-field">
-          <label htmlFor="user-sign">Username</label>
-          <input type="text" id="user-sign" required />
+          <label htmlFor="username" className="text-zinc-500 block mb-2">
+            Username
+          </label>
+          <input
+            type="text"
+            id="username"
+            className="border border-zinc-400 w-full h-10 rounded outline-0 px-2 mb-3"
+            required
+          />
         </div>
         <div className="input-field">
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" required />
+          <label htmlFor="email" className="text-zinc-500 block mb-2">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            className="border border-zinc-400 w-full h-10 rounded outline-0 px-2 mb-3"
+            required
+          />
         </div>
         <div className="input-field">
-          <label htmlFor="passWord">Password</label>
-          <input type="password" id="passWord" required />
+          <label htmlFor="password" className="text-zinc-500 block mb-2">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            className="border border-zinc-400 w-full h-10 rounded outline-0 px-2 mb-3"
+            required
+          />
         </div>
-        <button type="submit" className="btn-submit">
+
+        <button
+          type="submit"
+          className="bg-blue-500 text-white w-full h-10 rounded mt-4 mb-3.5"
+        >
           Sign Up
         </button>
       </form>
-      <p>
+      <p className="text-center text-sm font-light">
         Already have an account? <button onClick={toggleForm}>Sign in </button>
       </p>
     </div>
